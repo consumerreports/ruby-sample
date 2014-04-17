a794ee564f481b7964def1ca8f45774660449845
Gui Weinmann <gui@alphabetware.comg>
2014-04-17 00:46:28 -0400

    At last, static images the Sinatra way

--- web.rb
+++ web.rb
@@ -1,11 +1,14 @@
 require 'sinatra'
 set :public_folder, File.dirname(__FILE__) + '/public'
-use Rack::Static, :urls => ['/stylesheets', '/javascripts', '/images'], :root => settings.public_folder
+#use Rack::Static, :urls => ['/stylesheets', '/javascripts', '/images'], :root => settings.public_folder
 
-get '/' do
-	send_file File.expand_path('index.html', settings.public_folder)
-end
+#class Web < Sinatra::Base
+  set :static, true
+  get '/' do
+  	send_file File.expand_path('index.html', settings.public_folder)
+  end
 
-get '/local_env' do
-  ENV.collect{|k,v| "#{k}=#{v}"}.join("<br/>")
-end
+  get '/local_env' do
+  #  ENV.collect{|k,v| "#{k}=#{v}"}.join("<br/>")
+  end
+#end
