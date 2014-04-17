e228253bf6c97e3b74e917fea2eb78da5a392ce6
Gui Weinmann <gui@alphabetware.comg>
2014-04-16 23:20:56 -0400

    Atlanta hyperlocal prototype, inital commit

--- web.rb
+++ web.rb
@@ -1,5 +1,6 @@
 require 'sinatra'
+set :public_folder, File.dirname(__FILE__) + '/public'
 
 get '/' do
-  "Hello, Consumer Reports"
+	send_file File.expand_path('index.html', settings.public_folder)
 end
