d0ba6ad86f05d790396ac16aa7229ebf4ce48921
Gui Weinmann <gui@alphabetware.comg>
2014-04-17 00:34:39 -0400

    fix local_env route

--- web.rb
+++ web.rb
@@ -1,12 +1,11 @@
 require 'sinatra'
-use Rack::Static, :urls => ['/stylesheets', '/javascripts', '/images'], :root => 'public'
-
 set :public_folder, File.dirname(__FILE__) + '/public'
+use Rack::Static, :urls => ['/stylesheets', '/javascripts', '/images'], :root => settings.public_folder
 
 get '/' do
 	send_file File.expand_path('index.html', settings.public_folder)
 end
 
-get 'local_env' do
+get '/local_env' do
   ENV.collect{|k,v| "#{k}=#{v}"}.join("<br/>")
 end
