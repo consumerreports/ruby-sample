4ef849f5234ed4386b8b6ecf267b3f3d458511e0
Gui Weinmann <gui@alphabetware.comg>
2014-04-17 00:28:33 -0400

    Add local_env route, USE WITH CAUTION

--- web.rb
+++ web.rb
@@ -6,3 +6,7 @@ set :public_folder, File.dirname(__FILE__) + '/public'
 get '/' do
 	send_file File.expand_path('index.html', settings.public_folder)
 end
+
+get 'local_env' do
+  ENV.collect{|k,v| "#{k}=#{v}"}.join("<br/>")
+end
