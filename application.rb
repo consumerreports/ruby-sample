3351a6270d702cc0a9dbc6e0915b628d20f73f43
Gui Weinmann <gui@alphabetware.comg>
2014-04-23 12:36:56 -0400

    redirect for unbounce

--- application.rb
+++ application.rb
@@ -9,6 +9,10 @@ class WebApp < Sinatra::Base
   get '/' do
   	send_file File.expand_path('index.html', settings.public_folder)
   end
+  
+  get '/unbounce' do
+    "<html><meta http-equiv='refresh' content='1;URL=http://unbouncepages.com/4d3d594e-c979-11e3-b71e-22000ac464fa/'><body><CENTER><img alt='Loading...' src='http://ec.consumerreports.org/ec/images/cro/ux/loading.gif' /><br/>Loading...</CENTER></body></html>"
+  end
 
   run! if app_file == $0
 end
