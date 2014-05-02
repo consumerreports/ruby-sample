35e79fb9b7743a3e099616e2c2c1c9475bfc00b5
Gui Weinmann <gui@alphabetware.comg>
2014-05-02 11:57:46 -0400

    Enable newrelic

--- application.rb
+++ application.rb
@@ -1,9 +1,8 @@
 require 'rubygems'
 require 'sinatra'
-# require 'debugger'
-
-configure :production do
-  require 'newrelic_rpm'
+require 'newrelic_rpm'
+configure :development do
+  require 'debugger'
 end
 
 class WebApp < Sinatra::Base
@@ -17,15 +16,15 @@ class WebApp < Sinatra::Base
   	send_file File.expand_path('index.html', settings.public_folder)
   end
   
-  get '/unbounce/:unbounce' do
+  get '/pages/:page' do
     urls = {
       'shoutout' => '4d3d594e-c979-11e3-b71e-22000ac464fa'
       }
     urls.collect do |key,val|
-      if(params[:unbounce]==key)
+      if(params[:page]==key)
         "<html><meta http-equiv='refresh' content='1;URL=http://unbouncepages.com/#{val}/'><body><CENTER><img alt='Loading...' src='http://ec.consumerreports.org/ec/images/cro/ux/loading.gif' /><br/>Loading...</CENTER></body></html>" 
       else # show a friendly 404
-        "<html><body><h1>#{params[:unbounce].capitalize} is not a valid route.</body></html>"
+        "<html><body><h1>#{params[:page].capitalize} is not a valid route.</body></html>"
       end
     end
   end
